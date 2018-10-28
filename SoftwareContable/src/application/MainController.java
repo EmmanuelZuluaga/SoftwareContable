b8f45bcf7b665d4c040439e95f9c44f63a39630b
juanmgarcia97 <1709+juanmgarcia97@users.noreply.github.com>
2018-10-27 19:51:36 -0500

    interfaz

--- SoftwareContable/src/application/MainController.java
+++ SoftwareContable/src/application/MainController.java
@@ -1,5 +1,73 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
 public class MainController {
+	
+	@FXML
+	private TextField txtActivosCorreinte;
+	
+	@FXML
+	private TextField txtActivosNoCorreinte;
+	
+	@FXML
+	private TextField txtPasivosCorreinte;
+	
+	@FXML
+	private TextField txtPasivosNoCorreinte;
+	
+	@FXML
+	private TextField txtPasivosPatrimonio;
+	
+	@FXML
+	private TextField txtActivos;
+	
+	@FXML
+	private GridPane gridActivosCorrientes;
+	
+	@FXML
+	private GridPane gridActivosNoCorrientes;
+	
+	@FXML
+	private GridPane gridPasivosCorrientes;
+	
+	@FXML
+	private GridPane gridPasivosNoCorrientes;
+	
+	@FXML
+	private GridPane gridPatrimonio;
+	
+	@FXML
+	private ScrollPane scrollActivosCorreintes;
+	
+	@FXML
+	private ScrollPane scrollActivosNoCorreintes;
+	
+	@FXML
+	private ScrollPane scrollPasivosCorreintes;
+	
+	@FXML
+	private ScrollPane scrollPasivosNoCorreintes;
+	
+	@FXML
+	private ScrollPane scrollPatrimonio;
+	
+	@FXML
+	private ComboBox<String> cbTipoCuenta;
+	
+	@FXML
+	private TextField txtTipoCuenta;
+	
+	@FXML
+	private Button agregar;
+	
 	
 }
